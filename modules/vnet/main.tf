29e33d1b5b135c3c68335de0eb38a306cff3aefb
sudha <sudha.sivaprakasam@gmail.com>
2024-06-14 06:31:10 +0000

    test

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -11,4 +11,25 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
   address_space       = var.vnet_address_space
+
+  tags = local.common_tags
+
+}
+resource "azurerm_subnet" "web_subnet" {
+  name                 = "${local.resource_name_prefix}-${var.web_subnet_name}"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.web_subnet_address
+}
+
+#local block start with locals
+locals {
+  owners               = var.business_devision
+  environment          = var.environment
+  resource_name_prefix = "${var.business_devision}-${var.environment}"
+  #sap-dev
+  common_tags = {
+    owners      = local.owners
+    environment = local.environment
+  }
 }
