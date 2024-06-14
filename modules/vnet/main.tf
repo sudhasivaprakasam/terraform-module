452db1c02f387acd9e5882dea983306d01595852
sudha <sudha.sivaprakasam@gmail.com>
2024-06-14 06:26:09 +0000

    test

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -11,3 +11,4 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
   address_space       = var.vnet_address_space
+}
